41c4a5e57803e3cdb75608fed8f559cf4212b919
subsevenphp <subsevenphp@gmail.com>
2023-03-14 23:53:09 +0800

    renamed index.html style.css app.js

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
-// Select the form and search input
-const form = document.querySelector('form');
+const data = [
+    {
+        "term": "navigation bar",
+        "translation": "导航栏"
+    },
+    {
+        "term": "status bar",
+        "translation": "状态栏"
+    },
+    {
+        "term": "tool bar",
+        "translation": "工具栏"
+    },
+    {
+        "term": "usability testing",
+        "translation": "可用性测试"
+    }
+];
+
+const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('#search');
+const resultsContainer = document.querySelector('#results');
 
-// Add an event listener to the form for when it's submitted
-form.addEventListener('submit', function(e) {
-    // Prevent the default form submission behavior
-    e.preventDefault();
-
-    // Get the search term from the input field
-    const searchTerm = searchInput.value;
-
-    // Use an API or database to get search results based on the search term
-    const searchResults = getSearchResults(searchTerm);
-
-    // Display the search results in the results div
-    displaySearchResults(searchResults);
-});
-// Function to get search results based on a search term
-function getSearchResults(searchTerm) {
-    // Replace this with code to fetch search results from an API or database
-    const results = [
-        'JavaScript',
-        'HTML',
-        'CSS',
-        'React',
-        'Node.js'
-    ];
-
-    // Filter the results based on the search term
-    const filteredResults = results.filter(result => result.toLowerCase().includes(searchTerm.toLowerCase()));
-
-    return filteredResults;
+function search() {
+    const query = searchInput.value.trim().toLowerCase();
+    const filteredData = data.filter(item => {
+        return item.term.toLowerCase().includes(query);
+    });
+    displayResults(query, filteredData);
 }
 
-// Function to display search results in the results div
-function displaySearchResults(searchResults) {
-    // Select the results div
-    const resultsDiv = document.querySelector('#results');
-
-    // Clear any previous search results
-    resultsDiv.innerHTML = '';
-
-    // If there are no search results, display a message
-    if (searchResults.length === 0) {
-        resultsDiv.innerHTML = '<p>No results found.</p>';
+function displayResults(query, results) {
+    resultsContainer.innerHTML = '';
+    const header = document.createElement('h2');
+    header.textContent = `Search results for "${query}":`;
+    resultsContainer.appendChild(header);
+    if (results.length === 0) {
+        resultsContainer.innerHTML += '<p>No results found.</p>';
         return;
     }
-
-    // Create an unordered list for the search results
-    const resultList = document.createElement('ul');
-
-    // Loop through the search results and create list items for each result
-    searchResults.forEach(result => {
+    const list = document.createElement('ul');
+    results.forEach(result => {
         const listItem = document.createElement('li');
-        listItem.textContent = result;
-        resultList.appendChild(listItem);
+        const termLink = document.createElement('a');
+        termLink.textContent = result.term;
+        termLink.addEventListener('click', () => {
+            termLink.textContent = result.translation;
+            termLink.classList.add('translated');
+        });
+        listItem.appendChild(termLink);
+        list.appendChild(listItem);
     });
-
-    // Add the list of search results to the results div
-    resultsDiv.appendChild(resultList);
+    resultsContainer.appendChild(list);
 }
+
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    search();
+});
